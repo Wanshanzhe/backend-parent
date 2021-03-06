314601fb16aad88bc1a8018ab3042b3e5e6cdf97
Wanshanzhe <37918255+Wanshanzhe@users.noreply.github.com>
2021-03-06 13:51:38 +0800

    init

---
+++ backend-common/src/test/java/com/mooc/meetingfilm/backend/common/BackendCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mooc.meetingfilm.backend.common;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
