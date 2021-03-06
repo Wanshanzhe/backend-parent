81c76b23e1a5f91d4e7c867bc1470696a8b859c3
Wanshanzhe <37918255+Wanshanzhe@users.noreply.github.com>
2021-03-06 16:21:14 +0800

    mybatisplus 整合

--- backend-common/src/test/java/com/mooc/meetingfilm/backend/common/BackendCommonApplicationTests.java
+++ backend-common/src/test/java/com/mooc/meetingfilm/backend/common/BackendCommonApplicationTests.java
@@ -1,13 +1,16 @@
 package com.mooc.meetingfilm.backend.common;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class BackendCommonApplicationTests {
+public class BackendCommonApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 
 }
