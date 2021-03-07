be4e091c0065bafc55250ca9a4b7056a22af3ee8
Wanshanzhe <37918255+Wanshanzhe@users.noreply.github.com>
2021-03-07 22:51:34 +0800

    2021.03.07

---
+++ backend-show/backend-show-provider/src/main/java/com/mooc/meetingfilm/provider/controller/providerController.java
@@ -0,0 +1,26 @@
+package com.mooc.meetingfilm.provider.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author by 完善者
+ * @date 2021/3/7 13:02
+ * @DESC
+ */
+
+@Slf4j
+@RestController
+public class providerController {
+
+    @Value("${server.port}")
+    private int port;
+
+    @RequestMapping(value = "/provider/sayhello")
+    public String providerSayHello(String message){
+        log.info("provider sayhello port:{}，message:{}",port,message);
+        return "Provvider sayhello port：" + port + ",message：" + message;
+    }
+}
