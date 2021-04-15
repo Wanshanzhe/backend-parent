2db58e5722352c14c1583f458958334c6d44f2b0
Wanshanzhe <37918255+Wanshanzhe@users.noreply.github.com>
2021-04-15 14:31:35 +0800

    2021.04.15

---
+++ beckend-apigw-zuul/src/main/java/com/mooc/meetingfilm/apigwzuul/BeckendApigwZuulApplication.java
@@ -0,0 +1,15 @@
+package com.mooc.meetingfilm.apigwzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class BeckendApigwZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeckendApigwZuulApplication.class, args);
+    }
+
+}
