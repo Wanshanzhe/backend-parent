5f86944d08d21529c1c088c96064bf57d03faa78
Wanshanzhe <37918255+Wanshanzhe@users.noreply.github.com>
2021-04-15 14:29:36 +0800

    2021.04.15

--- backend-show/backend-show-provider/src/main/java/com/mooc/meetingfilm/provider/controller/providerController.java
+++ backend-show/backend-show-provider/src/main/java/com/mooc/meetingfilm/provider/controller/providerController.java
@@ -2,8 +2,7 @@ package com.mooc.meetingfilm.provider.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author by 完善者
@@ -18,9 +17,19 @@ public class providerController {
     @Value("${server.port}")
     private int port;
 
-    @RequestMapping(value = "/provider/sayhello")
+    @RequestMapping(value = "/provider/sayhello",method = RequestMethod.GET)
     public String providerSayHello(String message) {
         log.info("provider sayhello port:{}，message:{}", port, message);
         return "Provvider sayhello port：" + port + ",message：" + message;
+    }
+
+    @RequestMapping(value = "/provider/{providerId}/sayhello",method = RequestMethod.POST)
+    public String postTest(
+                @RequestHeader("author")String author,
+                @PathVariable("providerId") String providerId,
+                @RequestBody String json){
+
+        log.error("provider sayhello port:{}，author:{}，providerId:{}，message:{}",port, author, providerId, json);
+        return "Provider sayhello port:" + port + "message:" + json;
     }
 }
